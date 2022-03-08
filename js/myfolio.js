6c4bc2bb4b24089152c9b2cd127b68eb6ca774e0
uddinmayraj <mayrajuddin03@gmail.com>
2022-03-08 22:38:50 +0600

    adding-slider

---
+++ js/myfolio.js
@@ -0,0 +1,6 @@
+$(function (){
+    $('.banner_slider').slick({
+        prevArrow : '<i class="fa-solid fa-arrow-left icons slider_icon"></i>',
+        nextArrow: '<i class="fa-solid fa-arrow-right icons slider_icon"></i>'
+    })
+})
