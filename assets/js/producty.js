7511ef79ca8055f91d40755737f51f8e501c24f3
uddinmayraj <mayrajuddin03@gmail.com>
2022-04-09 07:23:55 +0600

    adding new

--- assets/js/producty.js
+++ assets/js/producty.js
@@ -1,9 +1,13 @@
 $(function(){
     $('#active_btn').on('click', function(){
         $('#nav_menu ul, #nav .logo').toggleClass('active')
-    })
+    });
     // $('.banner_slider').slick({
     //     prevArrow:'<i class="fa-solid fa-angle-left bIcon"></i>',
     //     nextArrow : '<i class="fa-solid fa-angle-right bIcon"></i>'
     // })
+    $('.counter').counterUp({
+        delay: 30,
+        time: 1000
+    });
 })
