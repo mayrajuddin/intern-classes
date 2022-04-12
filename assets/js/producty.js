b6148b4a4f26768c4f48e4a342b01dbce7b85bc6
uddinmayraj <mayrajuddin03@gmail.com>
2022-04-12 06:55:55 +0600

    fixed some problem

--- assets/js/producty.js
+++ assets/js/producty.js
@@ -1,14 +1,15 @@
 $(function(){
     $('#active_btn').on('click', function(){
         $('#nav_menu ul, #nav .logo').toggleClass('active')
-    })
+    });
     $('.banner_slider').slick({
-        prevArrow:'<i class="fa-solid fa-angle-left bIcon"></i>',
-        nextArrow : '<i class="fa-solid fa-angle-right bIcon"></i>'
-    })
+        arrows:false
+        // prevArrow:'<i class="fa-solid fa-angle-left bIcon"></i>',
+        // nextArrow : '<i class="fa-solid fa-angle-right bIcon"></i>'
+    });
     $('.counter').counterUp({
         delay: 30,
         time: 1000
-    })
+    });
     $('.tesSlider').slick()
 })
