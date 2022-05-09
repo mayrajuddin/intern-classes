c6256ddceeab2bfe042154d94405cb12bd4d8c17
uddinmayraj <mayrajuddin03@gmail.com>
2022-05-09 23:25:12 +0600

    add

--- assets/js/producty.js
+++ assets/js/producty.js
@@ -3,13 +3,28 @@ $(function(){
         $('#nav_menu ul, #nav .logo').toggleClass('active')
     });
     $('.banner_slider').slick({
-        arrows:false
+        
+        arrows:false,
         // prevArrow:'<i class="fa-solid fa-angle-left bIcon"></i>',
         // nextArrow : '<i class="fa-solid fa-angle-right bIcon"></i>'
+        
     });
+   
     $('.counter').counterUp({
         delay: 30,
         time: 1000
     });
-    $('.tesSlider').slick()
+    $('.tesSlider').slick({
+        slidesToShow:1,
+        slidesToScroll: 1,
+        asNavFor: '.profile_nav',
+        fade:true,
+        arrows:false
+    })
+    $('.profile_nav').slick({
+        slidesToShow:3,
+        asNavFor:'.tesSlider',
+        centerMode: true,
+        centerPadding:'0'
+    })
 })
