925c9a6ad0c94ff47999a0e667069441e405d242
uddinmayraj <mayrajuddin03@gmail.com>
2022-05-21 00:49:39 +0600

    add

--- assets/js/producty.js
+++ assets/js/producty.js
@@ -20,6 +20,10 @@ $(function(){
     });
     $('.blog_slider').slick({
         slidesToShow:3,
+        centerMode:true,
+        centerPadding:'0',
+        autoplay:true,
+        arrows:false,
         responsive: [
             {
               breakpoint: 768,
